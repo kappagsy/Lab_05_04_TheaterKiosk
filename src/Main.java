6934e9a58383757900c7051e29f5462aea1ead04
kappagsy <kappagsy@mail.uc.edu>
2022-09-22 13:33:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int personAge = 25;
+
+        System.out.println("What is your age?");
+
+       if (personAge >= 21)
+       {
+           System.out.println("You get a wristband!");
+       }
+    }
+}
